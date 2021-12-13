3197695a1da8097b74d37a795559ee8fc6e76e44
Víctor Mardones <v.mardones04@ufromail.cl>
2021-12-13 14:50:18 -0300

    Soluciona la evaluación de imágenes

--- app/main.py
+++ app/main.py
@@ -1,13 +1,12 @@
-import cv2
 from io import BytesIO
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
 from PIL import Image
-import requests
-from tensorflow import keras, reshape
-from tensorflow_hub import KerasLayer
-import uvicorn
+from pydantic import BaseModel
+import tensorflow as tf
+import tensorflow_hub as hub
+from urllib import request
 
 app = FastAPI()
 app.add_middleware(
@@ -18,40 +17,41 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 def index():
     return {"mensaje": "Página de inicio de la API del reconocedor de dibujos"}
 
 
+class Imagen(BaseModel):
+    uri: str
+
+
 @app.post('/predict')
-def predict(imagen: UploadFile = File(...)):
-    modelo = importar_modelo("../model/model.h5")
-    return categorizar(modelo, imagen)
+def predict(imagen: Imagen):
+    modelo = importar_modelo("model/model.h5")
+    img = leer_imagen(imagen.uri)
+    return evaluar(modelo, img)
 
 
 def importar_modelo(ruta):
-    return keras.models.load_model(ruta, custom_objects={'KerasLayer': KerasLayer})
+    return tf.keras.models.load_model(ruta, custom_objects={'KerasLayer': hub.KerasLayer})
 
 
-def categorizar(modelo, imagen):
-    respuesta = requests.get(imagen)
-    img = Image.open(BytesIO(respuesta.content))
+def leer_imagen(data_uri):
+    respuesta = request.urlopen(data_uri).read()
+    return Image.open(BytesIO(respuesta))
 
+
+def evaluar(modelo, img):
     # Elimina el canal alpha, solo deja RGB
     img = img.convert('RGB')
     img = np.array(img).astype(float) / 255
 
-    print('Tamaño original:', img.shape)
-
-    # Cambia el tamaño de la imagen
-    img = cv2.resize(img, (224, 224), interpolation=cv2.INTER_AREA)
-
-    print('Tamaño nuevo:', img.shape)
-
-    prediccion = modelo.predict(reshape(img, [-1, 224, 224, 3]))
+    prediccion = modelo.predict(tf.reshape(img, [-1, 224, 224, 3]))
     resultado = np.argmax(prediccion[0], axis=-1)
 
     categorias = ["data_table", "image", "radio_button_checked", "slider", "text_area"]
     print("Es " + categorias[resultado])
 
-    return resultado
+    return categorias[resultado]
